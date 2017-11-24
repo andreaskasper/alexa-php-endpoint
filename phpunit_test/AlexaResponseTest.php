c77119d924a806e7c2f34f60f1726b708181c3cb
Andreas Kasper <andreas.kasper@goo1.de>
2017-11-25 00:42:19 +0100

    * Erster Unittest für helloworld01

---
+++ phpunit_test/AlexaResponseTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *
+ *
+ *
+ *
+ * @license   http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ */
+namespace Alexa\Test;
+use PHPUnit\Framework\TestCase;
+
+
+final class AlexaResponseTest extends TestCase {
+	
+	public function testhelloworld01() {
+		try {
+			$w = new \Alexa\Endpoint\Response()
+			$w->say("Dies ist ein Test");
+			$w->send();
+			$this->assertEquals(true, true); //No Error, that it's a success
+		} catch (Exception $ex) {
+				$this->fail("Failed at helloworld01");
+		}		
+	}
+
+	
+	
+}
